d9a8f33d2d49b08094e76307ff7882656b6e6566
Deafhawk36 <albrechtjess@gmail.com>
2015-05-22 12:20:07 -0700

    BattleState

---
+++ FinalProj/Assets/Assets/scripts/States/BattleState.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using FSM;
+
+namespace CharacterWeaponFramework
+{
+    public class BattleState : FSMState
+    {
+        public BattleState()
+        {
+            _stateID = StateID.BattleStateID;
+        }
+
+        public override void Reason(GameObject player, GameObject npc)
+        {
+            AICharacterControl ai = npc.GetComponent<AICharacterControl>();
+            if (Vector3.Distance(player.transform.position, npc.transform.position) < ai.PersonalSpace)
+            {
+                ai.SetTransition(Transition.TransitionToBattleState);
+            }
+        }
+
+        public override void Act(GameObject player, GameObject npc)
+        {
+            ThirdPersonUserControl plyr = player.GetComponent<ThirdPersonUserControl>();
+            ThirdPersonCharacter character = player.GetComponent<ThirdPersonCharacter>();
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
