7df741508c52908e49ea9b25832e63848785a9b7
Deafhawk36 <albrechtjess@gmail.com>
2015-05-29 16:42:36 -0700

    Working Battle State but Sliding Characters

--- FinalProj/Assets/Assets/scripts/GlobalConsts.cs
+++ FinalProj/Assets/Assets/scripts/GlobalConsts.cs
@@ -9,6 +9,6 @@ namespace Globals
     {
         public const int MAX_GROUP_SIZE = 5;
         public const int NUM_MILLISECOND_IN_SECOND = 1000;
-        public const int WORLD_SIZE = 45;
+        public const int WORLD_SIZE = 15;
     }
 }
